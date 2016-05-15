748c278689a0d9623a7b7cb75eeaa2804b872e0e
Birgit Kühbacher <kuehbabi@googlemail.com>
2016-05-15 21:58:21 +0200

    Computing Module Array in DigitalPart + different Modi, MechanicalPart/DigitalPart/PilotInterface. Lights and Sensors deleted

--- LandingGearSystem/DigitalPart.cs
+++ LandingGearSystem/DigitalPart.cs
@@ -8,115 +8,202 @@ namespace LandingGearSystem
 {
     using SafetySharp.Modeling;
 
+    /// <summary>
+    /// Modus the digital part operates in.
+    /// </summary>
+    public enum Mode
+    {
+        /// <summary>
+        /// Any one value of the computing module has to be true to return a true value (logical OR).
+        /// </summary>
+        Any,
+        /// <summary>
+        /// All values of the computing module has to be true to return a true value (logical AND).
+        /// </summary>
+        All,
+// --> Necessary??
+        /// <summary>
+        /// Only one computing module is to be used.
+        /// </summary>
+        One
+    }
+
     class DigitalPart : Component
     {
         /// <summary>
-        /// Value from computing module one indicating whether the general electro valve is to be stimulated.
+        /// Indicates the modus the digital part is operating in.
         /// </summary>
-        public extern bool GeneralEVOne();
-        /// <summary>
-        /// Value from computing module one indicating whether the door closure electro valve is to be stimulated.
-        /// </summary>
-        public extern bool CloseEVOne();
-        /// <summary>
-        /// Value from computing module one indicating whether the door opening electro valve is to be stimulated.
-        /// </summary>
-        public extern bool OpenEVOne();
-        /// <summary>
-        /// Value from computing module one indicating whether the gear retraction electro valve is to be stimulated.
-        /// </summary>
-        public extern bool RetractEVOne();
-        /// <summary>
-        /// Value from computing module one indicating whether the gear extension electro valve is to be stimulated.
-        /// </summary>
-        public extern bool ExtendEVOne();
+        private Mode _mode;
 
         /// <summary>
-        /// Value from computing module two indicating whether all three gears are locked down.
+        /// Array with computing modules the digital part is composed of.
         /// </summary>
-        public extern bool GearsLockedDownOne();
-        /// <summary>
-        /// Value from computing module two indicating whether all three gears are maneuvering.
-        /// </summary>
-        public extern bool GearsManeuveringOne();
-        /// <summary>
-        /// Value from computing module two indicating whether an anomaly has been detected.
-        /// </summary>
-        public extern bool AnomalyOne();
+        public ComputingModule[] ComputingModules;
 
         /// <summary>
-        /// Value from computing module two indicating whether the general electro valve is to be stimulated.
+        /// Initializes a new instance.
         /// </summary>
-        public extern bool GeneralEVTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether the door closure electro valve is to be stimulated.
-        /// </summary>
-        public extern bool CloseEVTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether the door opening electro valve is to be stimulated.
-        /// </summary>
-        public extern bool OpenEVTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether the gear retraction electro valve is to be stimulated.
-        /// </summary>
-        public extern bool RetractEVTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether the gear extension electro valve is to be stimulated.
-        /// </summary>
-        public extern bool ExtendEVTwo();
+        /// <param name="mode">Indicates the mode the digital part is operating in: Any, All, One.</param>
+        /// <param name="count">Indicates how many computing modules are to be used.</param>
+        public DigitalPart(Mode mode, int count)
+        {
+            _mode = mode;
+            ComputingModules = new ComputingModule[count];
+            for(int i = 0; i< count; i++)
+            {
+                ComputingModules[i] = new ComputingModule();
+            }
+        }
 
-        /// <summary>
-        /// Value from computing module two indicating whether all three gears are locked down.
-        /// </summary>
-        public extern bool GearsLockedDownTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether all three gears are maneuvering.
-        /// </summary>
-        public extern bool GearsManeuveringTwo();
-        /// <summary>
-        /// Value from computing module two indicating whether an anomaly has been detected.
-        /// </summary>
-        public extern bool AnomalyTwo();
+        public override void Update()
+        {
+            foreach (ComputingModule module in ComputingModules)
+                Update(module);           
+        }
 
         /// <summary>
         /// Gets a value indicating whether the general electro valve is to be stimulated through composition of the two computing modules outputs with a logical or.
         /// </summary>
-        public bool GeneralEVComposition() => GeneralEVOne() || GeneralEVTwo();
+        public bool GeneralEVComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.GeneralEV == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.GeneralEV == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].GeneralEV;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether the door closure electro valve is to be stimulated through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool CloseEVComposition() => CloseEVOne() || CloseEVTwo();
+        public bool CloseEVComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.CloseEV == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.CloseEV == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].CloseEV;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether the door opening electro valve is to be stimulated through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool OpenEVComposition => OpenEVOne() || OpenEVTwo();
+        public bool OpenEVComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.OpenEV == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.OpenEV == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].OpenEV;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether the gear retraction electro valve is to be stimulated through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool RetractEVComposition() => RetractEVOne() || RetractEVTwo();
+        public bool RetractEVComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.RetractEV == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.RetractEV == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].RetractEV;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether the gear extension electro valve is to be stimulated through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool ExtendEVComposition() => ExtendEVOne() || ExtendEVTwo();
+        public bool ExtendEVComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.ExtendEV == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.ExtendEV == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].ExtendEV;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether all three gears are locked down through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool GearsLockedDownComposition() => GearsLockedDownOne() || GearsLockedDownTwo();
+        public bool GearsLockedDownComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.GearsLockedDown == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.GearsLockedDown == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].GearsLockedDown;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether all three gears are maneuvering through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool GearsManeuveringComposition() => GearsManeuveringOne() || GearsManeuveringTwo();
+        public bool GearsManeuveringComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.GearsManeuvering == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.GearsManeuvering == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].GearsManeuvering;
+                default:
+                    return false;
+            }
+        }
 
         /// <summary>
         /// Gets a value indicating whether an anomaly has been detected through composition of the two computing modules outputs with a logical or.
         /// </summary
-        public bool AnomalyComposition() => AnomalyOne() || AnomalyTwo();
+        public bool AnomalyComposition()
+        {
+            switch ((int)_mode)
+            {
+                case (int)Mode.All:
+                    return ComputingModules.All(element => element.Anomaly == true);
+                case (int)Mode.Any:
+                    return ComputingModules.Any(element => element.Anomaly == true);
+                case (int)Mode.One:
+                    return ComputingModules[0].Anomaly;
+                default:
+                    return false;
+            }
+        }
 
     }
 }
