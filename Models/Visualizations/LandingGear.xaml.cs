6558dce6bfc104ba31fe4e77365105a15025a46c
Birgit Kühbacher <kuehbabi@googlemail.com>
2016-06-19 22:39:05 +0200

    Visualization updated with SimulationControl and UpdateModelState method but exception is thrown

--- Models/Visualizations/LandingGear.xaml.cs
+++ Models/Visualizations/LandingGear.xaml.cs
@@ -18,6 +18,7 @@ namespace SafetySharp.CaseStudies.Visualizations
     using System.ComponentModel;
     using System.Runtime.CompilerServices;
     using Analysis;
+    using Infrastructure;
     using Modeling;
     using SafetySharp.CaseStudies.LandingGear.Modeling;
 
@@ -26,7 +27,7 @@ namespace SafetySharp.CaseStudies.Visualizations
     /// </summary>
     public partial class LandingGear : INotifyPropertyChanged
     {
-        private readonly Model _model;
+        private readonly Model _model = new Model(new InitializeOne());
 
         public HandlePosition MoveToPosition { get; private set; }
 
@@ -34,26 +35,61 @@ namespace SafetySharp.CaseStudies.Visualizations
         {
             InitializeComponent();
 
-            _model = new Model(new InitializeOne());
             _model.Faults.SuppressActivations();
-
-            txtSequence.DataContext = _model.DigitalPart.ComputingModules[0];
 
             MoveToPosition = _model.Cockpit.PilotHandle.Position == HandlePosition.Up ? HandlePosition.Down : HandlePosition.Up;
             HandleButtonText.DataContext = this;
 
-            txtHandle.DataContext = _model.Cockpit.PilotHandle;
+            SimulationControls.ModelStateChanged += (o, e) => UpdateModelState();
+            SimulationControls.SetModel(new Model(new InitializeOne()));
 
-            txtGeneralEV.DataContext = _model.MechanicalPartControllers.GeneralEV;
+            UpdateModelState();
 
         }
 
+        private void UpdateModelState()
+        {
+           //Actionsequence State
+            txtSequence.Text = _model.DigitalPart.ComputingModules[0].ActionSequenceState.ToString();
 
+            //Pilot Handle
+            txtHandle.Text = _model.Cockpit.PilotHandle.Position.ToString();
+
+            //General EV
+            txtGeneralEV.Text = _model.MechanicalPartControllers.GeneralEV.State.ToString();
+
+            //Doors
+            txtOpenEV.Text = _model.MechanicalPartControllers.OpenEV.State.ToString();
+            txtCloseEV.Text = _model.MechanicalPartControllers.CloseEV.State.ToString();
+            txtExtensionDoors.Text = $"Enabled: {_model.MechanicalPartControllers.ExtensionCircuitDoors.IsEnabled}, Pressure: {_model.MechanicalPartControllers.ExtensionCircuitDoors.Pressure}";
+            txtRetractionDoors.Text = $"Enabled: {_model.MechanicalPartControllers.RetractionCircuitDoors.IsEnabled}, Pressure: {_model.MechanicalPartControllers.RetractionCircuitDoors.Pressure}";
+            txtFrontDoorCyl.Text = _model.MechanicalActuators.FrontDoorCylinder.DoorCylinderState.ToString();
+            txtLeftDoorCyl.Text = _model.MechanicalActuators.LeftDoorCylinder.DoorCylinderState.ToString();
+            txtRightDoorCyl.Text = _model.MechanicalActuators.RightDoorCylinder.DoorCylinderState.ToString();
+            txtFrontDoor.Text = _model.MechanicalPartPlants.DoorFront.State.ToString();
+            txtLeftDoor.Text = _model.MechanicalPartPlants.DoorLeft.State.ToString();
+            txtRightDoor.Text = _model.MechanicalPartPlants.DoorRight.State.ToString();
+
+            //Gears
+            txtExtendEV.Text = _model.MechanicalPartControllers.ExtendEV.State.ToString();
+            txtRetractEV.Text = _model.MechanicalPartControllers.RetractEV.State.ToString();
+            txtExtensionGears.Text = $"Enabled: {_model.MechanicalPartControllers.ExtensionCircuitGears.IsEnabled}, Pressure: {_model.MechanicalPartControllers.ExtensionCircuitGears.Pressure}";
+            txtRetractionGears.Text = $"Enabled: {_model.MechanicalPartControllers.RetractionCircuitGears.IsEnabled}, Pressure: {_model.MechanicalPartControllers.RetractionCircuitGears.Pressure}";
+            txtFrontGearCyl.Text = _model.MechanicalActuators.FrontGearCylinder.GearCylinderState.ToString();
+            txtLeftGearCyl.Text = _model.MechanicalActuators.LeftGearCylinder.GearCylinderState.ToString();
+            txtRightGearCyl.Text = _model.MechanicalActuators.RightGearCylinder.GearCylinderState.ToString();
+            txtFrontGear.Text = _model.MechanicalPartPlants.GearFront.State.ToString();
+            txtLeftGear.Text = _model.MechanicalPartPlants.GearLeft.State.ToString();
+            txtRightGear.Text = _model.MechanicalPartPlants.GearRight.State.ToString();
+
+        }
+   
         private void HandleClicked(object sender, RoutedEventArgs e)
         {
             _model.Pilot.Move = MoveToPosition;
             MoveToPosition = MoveToPosition == HandlePosition.Up ? HandlePosition.Down : HandlePosition.Up;
             NotifyPropertyChanged("MoveToPosition");
+            UpdateModelState();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
